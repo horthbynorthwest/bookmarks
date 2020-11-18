b9654b9e0b5f704c84f808a9679e535b8187059f
Georgie Horth <georgiehorth@gmail.com>
2020-11-18 14:18:17 +0000

    step 11 done

--- spec/bookmark_spec.rb
+++ spec/bookmark_spec.rb
@@ -1,4 +1,6 @@
 require 'bookmark'
+require 'database_helpers'
+
 
 describe Bookmark do
   describe '.all' do
@@ -6,22 +8,28 @@ describe Bookmark do
       connection = PG.connect(dbname: 'bookmark_manager_test')
 
       # Add the test data
-      connection.exec("INSERT INTO bookmarks (url) VALUES ('http://www.makersacademy.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
-      connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+      bookmark =  Bookmark.create(url:'http://www.makersacademy.com', title: 'Makers')
+      Bookmark.create(url:'http://www.destroyallsoftware.com', title: 'Destroy All Software')
+      Bookmark.create(url: 'http://www.google.com', title: 'Google')
       bookmarks = Bookmark.all
 
-      expect(bookmarks).to include("http://www.makersacademy.com")
-      expect(bookmarks).to include("http://www.destroyallsoftware.com")
-      expect(bookmarks).to include("http://www.google.com")
+      expect(bookmarks.length).to eq 3
+      expect(bookmarks.first).to be_a Bookmark
+      expect(bookmarks.first.id).to eq bookmark.id
+      expect(bookmarks.first.title).to eq 'Makers'
+      expect(bookmarks.first.url).to eq 'http://www.makersacademy.com'
     end
   end
 
   describe '.create' do
     it 'creates a new bookmark' do
-      Bookmark.create(url: 'http://twitter.com')
+      bookmark = Bookmark.create(url: 'http://twitter.com', title: 'Twitter')
+      persisted_data = persisted_data(id: bookmark.id)
 
-      expect(Bookmark.all).to include 'http://twitter.com'
+      expect(bookmark).to be_a Bookmark
+      expect(bookmark.id).to eq persisted_data['id'] 
+      expect(bookmark.url).to eq 'http://twitter.com'
+      expect(bookmark.title).to eq 'Twitter'
     end
   end
 end
--- spec/features/adding_bookmarks_spec.rb
+++ spec/features/adding_bookmarks_spec.rb
@@ -4,8 +4,9 @@ feature 'user can add a bookmark' do
   scenario 'A user can add a bookmark to Bookmark Manager' do
     visit('/bookmarks/new')
     fill_in('url', with: 'http://twitter.com')
+    fill_in('title', with: 'Twitter')
     click_button('Submit')
 
-    expect(page).to have_content 'http://twitter.com'
+    expect(page).to have_link('Twitter', href: 'http://twitter.com')
   end
-end 
+end
